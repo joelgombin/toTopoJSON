1a631d2ea4a23878f0e6d140165991cf8951b51f
Joel Gombin <joel.gombin@gmail.com>
2014-04-06 16:55:08 +0200

    namespace + reprojection

--- R/toTopoJSON.R
+++ R/toTopoJSON.R
@@ -1,3 +1,4 @@
+
 #' Transform one or several SpatialPolygonsDataFrame into a TopoJSON file
 #'
 #' \code{toTopoJSON} takes one or several \code{SpatialPolygonsDataFrame} objects and creates a topojson file, multi-layered if needed. This functions needs the \code{topojson} CLI utility to be installed (see \link{https://github.com/mbostock/topojson/wiki/Installation}). 
@@ -14,15 +15,17 @@
 #' 
 #' @return Nothing. As a side effect, a topojson file is created. 
 #' @export
+#' @import rgdal sp
 #' @references Mike Bostock's wiki: \link{https://github.com/mbostock/topojson/wiki}.
 
 toTopoJSON <- function(SPDF, path=getwd(), filename=NULL, simplification=1, quantisation="1e4", width=800, height=400, properties=NULL, id=NULL) {
   require(rgdal)
   tmpdir <- tempdir()
   if (is.null(filename)) filename <- names(SPDF)[1]
-  setCPLConfigOption("SHAPE_ENCODING", "UTF-8")
+    setCPLConfigOption("SHAPE_ENCODING", "UTF-8")
   lapply(names(SPDF), function(x) {
     writeOGR(get(x), dsn=tmpdir, layer=x, driver="ESRI Shapefile", overwrite_layer=TRUE)
+    assign(x, spTransform(get(x), CRS("+proj=merc +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")))
   })  
   call <- paste("topojson -o '", path,"/", filename,".json' -q ",as.character(quantisation) , ifelse(is.null(id), "", paste0(" --id-property ", id))  , " -s ", simplification," --width ",width," --height ", height, " --shapefile-encoding utf8", ifelse(is.null(properties), " -p", ifelse(paste(properties, collapse="") %in% "", "", paste(" -p", properties, collapse=""))), " -- ", paste("'", tmpdir, "/", names(SPDF), ".shp'",sep="", collapse=" "),  sep="")
   cat(call)
